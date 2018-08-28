6b90b9f9b86010ea02f31c86e02d327a3ecb895c
Bryan Norden <bryan.norden@yahoo.com>
2018-08-28 12:28:29 -0600

    Created dynamic hello world get request that took in a parameter and appended it after hello

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,4 +12,13 @@ public func routes(_ router: Router) throws {
         
     }
     
+    // http://localhost:8080/hello/<someString> -> Hello, <someString>!
+    
+    router.get("hello", String.parameter) { request -> String in
+        
+        let someString = try request.parameters.next(String.self)
+        
+        return "Hello, \(someString)!"
+        
+    }
 }
