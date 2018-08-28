64cb66eb6dc9b381647205521108bd7b4a35398d
Bryan Norden <bryan.norden@yahoo.com>
2018-08-28 12:27:55 -0600

    Created static get request that returns Hello, world!

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,5 +4,12 @@ import Vapor
 public func routes(_ router: Router) throws {
     
 
+    // http://localhost:8080/hello -> Hello, world!
+    
+    router.get("hello") { request in
+        
+        return "Hello, world!"
+        
+    }
     
 }
